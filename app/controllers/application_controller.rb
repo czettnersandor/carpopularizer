caec0a41f1e9954fe78d97b04f31de19f3526bd9
Sándor Czettner <zoner@turulpc.localdomain>
2010-01-05 10:09:30 +0100

    Main layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,14 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  before_filter :set_toppings
   include AuthenticatedSystem
 
+  layout "main"
+
+  def set_toppings
+    @proba = "HELLO"
+  end
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
