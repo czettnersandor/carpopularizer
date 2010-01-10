340b5ff7c998dd1e0c27cfaf49401ef548cef6ce
zoner <zoner@localhost.localdomain>
2010-01-10 13:53:13 +0100

    More logic for password change

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -10,21 +10,19 @@ class ProfileController < ApplicationController
 
   def update
     @user = User.find(current_user)
-
-    if ((params[:user][:password] == params[:user][:password_confirmation]) && !params[:user][:password_confirmation].blank?)
-      current_user.password_confirmation = params[:password_confirmation]
-      current_user.password = params[:password]
-      if current_user.save
-        flash[:notice] = _("Password successfully updated.")
-      else
-        flash[:error] = _("An error occured, your password was not changed.")
-        render :action => 'edit'
-      end
-    end
-
     if @user.update_attributes(params[:user])
+      if ((params[:user][:password] == params[:user][:password_confirmation]) && !params[:user][:password_confirmation].blank?)
+        current_user.password_confirmation = params[:password_confirmation]
+        current_user.password = params[:password]
+        if current_user.save
+          flash[:notice] = _("Password successfully updated.")
+        else
+          flash[:error] = _("An error occured, your password was not changed.")
+          render :action => 'edit'
+        end
+      end
       # flash[:notice] = _('User profile was succesfully updated.')
-      redirect :action => "index"
+      redirect_to :action => "index"
     else
       render :action => "edit"
     end
