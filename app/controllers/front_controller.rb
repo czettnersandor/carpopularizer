8ac50444574454de71c7678026cb0d937479a001
zoner <zoner@localhost.localdomain>
2010-01-14 18:28:21 +0100

    News sorted by hit count

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,7 +1,9 @@
 class FrontController < ApplicationController
 
   def index
-    @title = _("Hírek")
+    @title = _("News")
+    @news = Rssnews.find :all, :order=>"created_at DESC", :limit=>5
+    @news_by_hits = Rssnews.find :all, :order=>"hits DESC", :limit=>5
     
   end
 
