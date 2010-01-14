46b6b98129f749abdf61afc5fc63f67e9ed93f08
Sándor Czettner <zoner@turulpc.localdomain>
2010-01-14 23:46:58 +0100

    Capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,20 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
-
-set :scm, :subversion
+set :application, "boxutca"
+set :repository, "git@eperpalanta.hu:boxutca.git"
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :deploy_via, :remote_cache
+role :web, "boxutca.eperpalanta.hu"
+role :app, "boxutca.eperpalanta.hu"
+role :db, "boxutca.eperpalanta.hu", :primary => true
+set :deploy_to, "/var/customers/eper/#{application}"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
-
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
