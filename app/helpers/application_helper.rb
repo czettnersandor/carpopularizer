ea02b2b81644c50fa321057002ee3b7e5b13c6b1
zoner <zoner@localhost.localdomain>
2010-01-15 12:39:35 +0100

    Shorten news description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,22 @@ module ApplicationHelper
   def current_url(params={})
     url_for :only_path=>false, :overwrite_params=>params
   end
+
+  def truncate_words(text, length = 5, end_string = ' …')
+    words = text.split()
+    words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+  end
+
+  def shorten (string, count = 200)
+    if string.length >= count
+      shortened = string[0, count]
+      splitted = shortened.split(/\s/)
+      words = splitted.length
+      splitted[0, words-1].join(" ") + ' ...'
+    else
+      string
+    end
+  end
+
+
 end
