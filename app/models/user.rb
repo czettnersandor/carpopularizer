84c8c37088f0fc15c0fa0daf5f8254a36a20fe0b
Sándor Czettner <zoner@turulpc.localdomain>
2010-01-15 18:22:45 +0100

    Menu fix for IE

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,8 @@ class User < ActiveRecord::Base
   validates_length_of       :email,    :within => 3..100
   validates_uniqueness_of   :login, :email, :case_sensitive => false
   before_save :encrypt_password
-  before_create :make_activation_code 
+  before_create :make_activation_code
+  
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
   attr_accessible :login, :email, :password, :password_confirmation,
