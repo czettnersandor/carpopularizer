1b7c773d7fdfac04d3067f7bdb9c860040a940d6
zoner <zoner@turulpc>
2010-03-03 17:51:15 +0100

    Design

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,6 +6,11 @@ class CarsController < ApplicationController
       :conditions => gen_conditions(params, Car), :include => [:carimages, :user])
 
     @top5 = Car.top5
+
+    @searchy = ""
+    [1900..Time.now.year].each do |s_year|
+      @searchy += "<option value=#{s_year}>#{s_year}</option>"
+    end
   end
 
 end
