95933f159e3f3a434f391c7bf4b825ea1bd9a705
zoner <zoner@turulpc>
2010-03-10 16:33:19 +0100

    Design issues

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100223145518) do
+ActiveRecord::Schema.define(:version => 20100309132319) do
 
   create_table "adverts", :force => true do |t|
     t.string   "title"
@@ -356,6 +356,14 @@ ActiveRecord::Schema.define(:version => 20100223145518) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "body"
+  end
+
+  create_table "userevents", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "message"
+    t.integer  "reply_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
