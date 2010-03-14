004793bbabc2f8098a5d0c6aabe34ec848fa6a15
zoner <zoner@turulpc>
2010-03-14 20:54:44 +0100

    Temp dir

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
-ENV['TMPDIR'] = '/srv/www/boxutca.com/htdocs/temp' if "production" == RAILS_ENV
+ENV['TMPDIR'] = '/srv/www/boxutca.com/htdocs/temp' if "production" == ENV['RAILS_ENV']
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
